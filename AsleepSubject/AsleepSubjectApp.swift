716553c8d7e7acdb83da58f8a0e89343383e2329
stevenkim18 <11882@naver.com>
2026-01-16 17:09:36 +0900

    Initial Commit

---
+++ AsleepSubject/AsleepSubjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsleepSubjectApp.swift
+//  AsleepSubject
+//
+//  Created by seungwooKim on 1/16/26.
+//
+
+import SwiftUI
+
+@main
+struct AsleepSubjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
